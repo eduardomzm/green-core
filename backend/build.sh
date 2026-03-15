bd6c335b879cb4394756110a7ba6f05abdb9b2f8
eduardomzm <eduardo.mezam@outlook.com>
2026-03-15 01:24:18 -0600

    script para crear admin en produccion

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,9 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+export DJANGO_SUPERUSER_USERNAME=greencore
+export DJANGO_SUPERUSER_EMAIL=admin@greencore.com
+export DJANGO_SUPERUSER_PASSWORD=greencore2026
+
+python manage.py createsuperuser --noinput || true
