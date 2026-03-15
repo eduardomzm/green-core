a1f1eb1ab1fd6d6ed37c657e5254a8bf2b412a6a
eduardomzm <eduardo.mezam@outlook.com>
2026-03-15 01:33:59 -0600

    forzar actualizacion de password de admin

--- backend/build.sh
+++ backend/build.sh
@@ -7,8 +7,17 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-export DJANGO_SUPERUSER_USERNAME=greencore
-export DJANGO_SUPERUSER_EMAIL=admin@greencore.com
-export DJANGO_SUPERUSER_PASSWORD=greencore2026
-
-python manage.py createsuperuser --noinput || true
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+user = User.objects.filter(username='greencore').first()
+if user:
+    user.set_password('Admin12345')
+    user.role = 'ADMIN'
+    user.is_superuser = True
+    user.is_staff = True
+    user.save()
+    print(' Contraseña y rol de Admin actualizados con éxito.')
+else:
+    print(' No se encontró el usuario admin.')
+"
