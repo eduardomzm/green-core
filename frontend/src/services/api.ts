9d37f1ea7318c211c83c9368f2267fef248f9900
eduardomzm <eduardo.mezam@outlook.com>
2026-03-17 23:54:17 -0600

    preperacion para produccion

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://www.greencore.com.mx/api/",
+  baseURL: "https://greencore-api-83ow.onrender.com/api/",
 });
 
 api.interceptors.request.use((config) => {
