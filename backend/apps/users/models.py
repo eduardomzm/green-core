48d7332de286421def2a2d34808418af96b9af91
eduardomzm <eduardo.mezam@outlook.com>
2026-03-18 03:25:31 -0600

    fix: agregar argumento from_email faltante en send_mail

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 from django.dispatch import receiver
 from django.urls import reverse
-from backend.config.settings import DEFAULT_FROM_EMAIL
 from django_rest_passwordreset.signals import reset_password_token_created
 from django.core.mail import send_mail  
 
