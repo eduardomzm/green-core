433a09000cecfaf544c35f450eb5f521980070d0
eduardomzm <eduardo.mezam@outlook.com>
2026-03-17 22:34:29 -0600

    configurar api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://green-core-backend.onrender.com/api/",
+  baseURL: "https://greencore.com.mx/api/",
 });
 
 api.interceptors.request.use((config) => {
